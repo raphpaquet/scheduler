64331a0dce700440cef8c69ed06967738d7fc5c7
Raphaelle Paquet <68092222+raphpaquet@users.noreply.github.com>
2020-12-12 13:28:45 -0500

    mode, transition, back function

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,32 @@
+
+import { useState } from 'react';
+
+
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial)
+  const [history, setHistory] = useState([initial])
+  
+  function transition(newMode, replace = false ) {
+
+    if (replace) {
+        setMode(newMode)
+        setHistory(prev => ([...prev.slice(0, prev.length -1), newMode]))
+      } else {
+        setMode(newMode)
+        setHistory(prev => ([...prev, newMode]));
+      }
+    }
+
+  function back() {
+    if (history.length > 1) {
+      setHistory(prev => ([...prev.slice(0, prev.length -1)]))
+      setMode(history.slice(0, history.length -1)[history.length -2])
+    }
+  }
+  return { mode, transition, back };
+}
+
+
+
+
